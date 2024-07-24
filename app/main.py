9b1582a204a4aefe0536827e5feb7c4dc975f950
yanruijie902136 <yanruijie902136@gmail.com>
2024-07-24 22:35:54 +0800

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,5 +1,107 @@
-# Uncomment this to pass the first stage
+#!/usr/bin/env python3
+# coding=utf-8
+
 import socket
+
+
+class HttpRequest:
+    def __init__(self):
+        self.__method = ""
+        self.__target = ""
+        self.__version = ""
+        self.__headers = {}
+        self.__body = ""
+
+    @staticmethod
+    def recvFromSocket(sock: socket.socket):
+        requestLines = sock.recv(1024).decode().split("\r\n")
+
+        request = HttpRequest()
+        request.__method, request.__target, request.__version = requestLines[0].split()
+        request.__body = requestLines[-1]
+        for headerLine in requestLines[1:]:
+            if not headerLine:
+                break
+            field, value = headerLine.split(":", maxsplit=1)
+            request.__headers[field] = value.lstrip().rstrip()
+
+        return request
+
+    @property
+    def method(self):
+        return self.__method
+
+    @property
+    def target(self):
+        return self.__target
+
+    @property
+    def version(self):
+        return self.__version
+
+    @property
+    def headers(self):
+        return self.__headers
+
+    @property
+    def body(self):
+        return self.__body
+
+    def __str__(self):
+        return (
+            f"{self.__method} {self.__target} {self.__version}\r\n" +
+            "".join([f"{key}: {value}\r\n" for key, value in self.__headers]) +
+            "\r\n" +
+            self.__body
+        )
+
+
+class HttpResponse:
+    def __init__(self):
+        self.__version = ""
+        self.__status = ""
+        self.__headers = {}
+        self.__body = ""
+
+    def sendToSocket(self, sock: socket.socket):
+        sock.sendall(str(self).encode())
+
+    @staticmethod
+    def handleRequest(request: HttpRequest):
+        response = HttpResponse()
+        response.__version = request.version
+
+        if request.target == "/user-agent":
+            response.__handleUserAgent(request)
+        elif request.target.startswith("/echo/"):
+            response.__handleEcho(request)
+        else:
+            response.__handleUrlPath(request)
+
+        return response
+
+    def __handleEcho(self, request: HttpRequest):
+        self.__status = "200 OK"
+        self.__body = request.target[6:]
+        self.__headers["Content-Type"] = "text/plain"
+        self.__headers["Content-Length"] = len(self.__body)
+
+    def __handleUrlPath(self, request: HttpRequest):
+        self.__status = "200 OK" if request.target == "/" else "404 Not Found"
+
+    def __handleUserAgent(self, request: HttpRequest):
+        self.__status = "200 OK"
+        self.__body = request.headers["User-Agent"]
+        self.__headers["Content-Type"] = "text/plain"
+        self.__headers["Content-Length"] = len(self.__body)
+
+    def __str__(self):
+        return (
+            f"{self.__version} {self.__status}\r\n" +
+            "".join([f"{key}: {value}\r\n" for key, value in self.__headers]) +
+            "\r\n" +
+            self.__body
+        )
 
 
 def main():
@@ -8,22 +110,14 @@ def main():
 
     # Uncomment this to pass the first stage
 
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    client_socket, _ = server_socket.accept() # wait for client
+    serverSocket = socket.create_server(("localhost", 4221), reuse_port=True)
+    clientSocket, _ = serverSocket.accept() # wait for client
 
-    data = client_socket.recv(1024)
-    request = data.decode()
-    request_lines = request.split("\r\n")
-    _, target, _ = request_lines[0].split()
-
-    if target.startswith("/echo/"):
-        echo_str = target[6:]
-        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo_str)}\r\n\r\n{echo_str}"
-    elif target == "/":
-        response = "HTTP/1.1 200 OK\r\n\r\n"
-    else:
-        response = "HTTP/1.1 404 Not Found\r\n\r\n"
-    client_socket.sendall(response.encode())
+    request = HttpRequest.recvFromSocket(clientSocket)
+    print(request)
+    response = HttpResponse.handleRequest(request)
+    print(response)
+    response.sendToSocket(clientSocket)
 
 
 if __name__ == "__main__":
