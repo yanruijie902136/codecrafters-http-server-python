420baab84401de5087962d5ef826b9251e71a576
yanruijie902136 <yanruijie902136@gmail.com>
2024-07-24 21:33:12 +0800

    pass stage

--- app/main.py
+++ app/main.py
@@ -10,7 +10,14 @@ def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, _ = server_socket.accept() # wait for client
-    client_socket.send(b"HTTP/1.1 200 OK\r\n\r\n")
+
+    data = client_socket.recv(1024)
+    request = data.decode()
+    request_lines = request.split("\r\n")
+    _, target, _ = request_lines[0].split()
+
+    response = "HTTP/1.1 200 OK\r\n\r\n" if target == "/" else "HTTP/1.1 404 Not Found\r\n\r\n"
+    client_socket.sendall(response.encode())
 
 
 if __name__ == "__main__":
