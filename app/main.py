b03fe8e3b0ae860d595356970987477fd7cfe243
yanruijie902136 <yanruijie902136@gmail.com>
2025-04-01 14:56:08 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,6 +6,19 @@ class HTTPClientConnection:
         self._reader = reader
         self._writer = writer
 
+    async def recv_request(self) -> None:
+        status_line = await self._reader.readuntil(b"\r\n")
+        print("status line =", status_line)
+        while True:
+            header = await self._reader.readuntil(b"\r\n")
+            if header == b"\r\n":
+                break
+            print("header =", header)
+
+    async def send_response(self) -> None:
+        self._writer.write(b"HTTP/1.1 200 OK\r\n\r\n")
+        await self._writer.drain()
+
     async def __aenter__(self) -> None:
         pass
 
@@ -23,7 +36,8 @@ class HTTPServer:
     async def _client_connected_cb(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
         connection = HTTPClientConnection(reader, writer)
         async with connection:
-            pass
+            await connection.recv_request()
+            await connection.send_response()
 
 
 if __name__ == "__main__":
