2b3bde7462d25b09a00034f903403ed1a752b86c
yanruijie902136 <yanruijie902136@gmail.com>
2025-04-01 17:57:25 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,19 @@
 import asyncio
+import dataclasses
+import typing
+
+
+@dataclasses.dataclass(frozen=True)
+class HTTPRequestLine:
+    method: str
+    target: str
+    version: str
+
+    @classmethod
+    async def parse(cls, reader: asyncio.StreamReader) -> typing.Self:
+        data = (await reader.readuntil(b"\r\n"))[:-2].decode()
+        method, target, version = data.split(" ")
+        return cls(method, target, version)
 
 
 class HTTPClientConnection:
@@ -6,17 +21,19 @@ class HTTPClientConnection:
         self._reader = reader
         self._writer = writer
 
-    async def recv_request(self) -> None:
-        status_line = await self._reader.readuntil(b"\r\n")
-        print("status line =", status_line)
+    async def recv_request(self) -> tuple[HTTPRequestLine, dict[str, str]]:
+        request_line = await HTTPRequestLine.parse(self._reader)
+        headers = {}
         while True:
-            header = await self._reader.readuntil(b"\r\n")
-            if header == b"\r\n":
-                break
-            print("header =", header)
+            data = (await self._reader.readuntil(b"\r\n"))[:-2].decode()
+            if not data:
+                return request_line, headers
+            i = data.index(":")
+            field_name, field_value = data[:i], data[i+1:].strip()
+            headers[field_name] = field_value
 
-    async def send_response(self) -> None:
-        self._writer.write(b"HTTP/1.1 200 OK\r\n\r\n")
+    async def send_response(self, response: bytes) -> None:
+        self._writer.write(response)
         await self._writer.drain()
 
     async def __aenter__(self) -> None:
@@ -36,8 +53,12 @@ class HTTPServer:
     async def _client_connected_cb(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
         connection = HTTPClientConnection(reader, writer)
         async with connection:
-            await connection.recv_request()
-            await connection.send_response()
+            request_line, _ = await connection.recv_request()
+            if request_line.target == "/":
+                response = b"HTTP/1.1 200 OK\r\n\r\n"
+            else:
+                response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+            await connection.send_response(response)
 
 
 if __name__ == "__main__":
